0145323e31d07e58e04b0eff00cab5d28874d9a3
finanovita <finanovita479@gmail.com>
2024-03-28 10:22:12 +0700

    step 2,3,4 q_short

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -49,6 +49,12 @@ void q_short(int low, int high)
     if (low > high) { //step 1
         return;
     }
+
+    pivot = arr[low]; //step 2
+    i = low + 1; //step 3
+    j = high;  //step 4
+
+
 }
 
 int main()
