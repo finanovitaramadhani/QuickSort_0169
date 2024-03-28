2453fef6c1a63485fab8441ce952d56b9af6aa02
finanovita <finanovita479@gmail.com>
2024-03-28 10:56:40 +0700

    panggilan recursive array q_short kiri dan kanan

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -82,6 +82,12 @@ void q_short(int low, int high)
         //swap the pivot element with the element at index j
         swap(low, j);
     }
+
+    //recursive call to start the left sub array
+    q_short(low, j - 1); //step 12
+
+    //recursive call to start the right sub array
+    q_short(j + 1, high); //step 13
 }
 
 int main()
