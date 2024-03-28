e93648031a1c40cf3d56d4c44fcf3033b167e2a5
finanovita <finanovita479@gmail.com>
2024-03-28 10:05:30 +0700

    Memasukan prosedure swap

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -33,6 +33,14 @@ void input() {
     }
 }
 
+//swaps the element at index x with the element at indexs y
+void swap(int x, int y)
+{
+    int temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+    mov_count++;
+}
 
 
 int main()
